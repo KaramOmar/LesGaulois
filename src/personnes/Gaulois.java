3f727e23b5378d8d1616b06406007669e99d0640
karam omar <krm3762a@ens.fsi>
2022-09-27 20:06:38 +0200

    premier comit

---
+++ src/personnes/Gaulois.java
@@ -0,0 +1,27 @@
+package personnes;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+}
